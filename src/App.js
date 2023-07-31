9a5c4264654a00212cb635bfb4f1c91e10792eb4
karthik66666 <karthikeyangt40@gmail.com>
2023-07-31 18:32:39 +0530

    react

--- src/App.js
+++ src/App.js
@@ -3,22 +3,66 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+    <body>
+      <div>
+        <h1>
+          Hi my name is karthikeyan welcome to my Profile
+         </h1>
+      </div>
+      <div>
+      
+     <table>
+           <tr>
+            <td>si:no</td>
+            <td>Education</td>
+            <td>Passed Out Year</td>
+          </tr>
+          <tbody>
+            <tr>
+            <td>
+              1
+            </td>
+            <td>
+              SSLC
+            </td>
+            <td>
+              2014
+            </td>
+          </tr>
+          <tr>
+            <td>
+              2.
+            </td>
+            <td>
+              Diploma in Mechanical Engenering
+            </td>
+            <td>
+              2017
+            </td>
+          </tr>
+          </tbody>
+     </table>
+        
+      </div>
+    </body>
+    </>
   );
 }
 
